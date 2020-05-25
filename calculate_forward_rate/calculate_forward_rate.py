8e2e2debf88e8a1a183282ffb292b23f8d070f21
fyyying <45222861+fyyying@users.noreply.github.com>
2020-05-25 13:02:12 +0000

    Add condition to choose scenarios to play with

--- calculate_forward_rate/calculate_forward_rate.py
+++ calculate_forward_rate/calculate_forward_rate.py
@@ -1,28 +1,39 @@
-rate_path = r"./Calculate fw data.csv"
-rate_path_group = r"./Calculate fw data group.csv"
-
-
 # Implementation with PySpark
 from pyspark.sql import SparkSession
 from pyspark.sql import Window
 from pyspark.sql import functions as f
 
+# Start with selecting the scenario to play with ["forward_rate", "forward_rate_with_group"]:
+# forward_rate: the simple example without currency group
+# forward_rate_group: the example when currency group is added
+select_scenario = "forward_rate" #"forward_rate_with_group"
+
+
+# Read in the data
+if select_scenario == "forward_rate":
+    rate_path = r"./Calculate fw data.csv"
+elif select_scenario == "forward_rate_with_group":
+    rate_path = r"./Calculate fw data group.csv"
+else:
+    raise ValueError("No valid scenario given")
+
 # Define Spark settings
 builder = SparkSession.builder.appName("Koalas")
-# builder = builder.config("spark.sql.execution.arrow.enabled", "true")\
-# .config("spark.sql.adaptive.enabled", "true")\
-# .config("spark.sql.optimizer.nestedSchemaPruning.enabled", "true")\
-# .config("spark.sql.optimizer.dynamicPartitionPruning", "true")\
+builder = builder.config("spark.sql.execution.arrow.enabled", "true")\
+.config("spark.sql.adaptive.enabled", "true")\
+.config("spark.sql.optimizer.nestedSchemaPruning.enabled", "true")\
+.config("spark.sql.optimizer.dynamicPartitionPruning", "true")\
 
 spark = builder.getOrCreate()
 
 # Read in the spot rate data
 data = spark.read.csv(rate_path, header=True, inferSchema=True)
-# data = spark.read.csv(rate_path_group, header=True, inferSchema=True)
 
 # Shift the discount factor by 1 period to forward the reserve to corresponding period
-window = Window.orderBy("Maturity")
-# window = Window.partitionBy("Currency").orderBy("Maturity")
+if select_scenario == "forward_rate_group":
+    window = Window.partitionBy("Currency").orderBy("Maturity")
+else:
+    window = Window.orderBy("Maturity")
 data = data.withColumn("Spot_Rate_Shift", f.lag(f.col("Spot_Rate"), 1).over(window)) \
            .fillna(0, subset=["Spot_Rate_Shift"])
 data = data.withColumn("Forward_Rate", (1 + f.col("Spot_Rate"))**f.col("Maturity") /
@@ -33,11 +44,12 @@ import pandas as pd
 
 # Read in the data from csv
 data = pd.read_csv(rate_path)
-# data = pd.read_csv(rate_path_group)
 
 # Shift the spot rate down along the maturity axis and calculate forward rate
-# data["Spot_Rate_Shift"] = data.groupby("Currency")["Spot_Rate"].shift(1, fill_value=0)
-data["Spot_Rate_Shift"] = data["Spot_Rate"].shift(1, fill_value=0)
+if select_scenario == "forward_rate_group":
+    data["Spot_Rate_Shift"] = data.groupby("Currency")["Spot_Rate"].shift(1, fill_value=0)
+else:
+    data["Spot_Rate_Shift"] = data["Spot_Rate"].shift(1, fill_value=0)
 data["Forward_Rate"] = (((1 + data["Spot_Rate"]) ** data["Maturity"]) /
                        ((1 + data["Spot_Rate_Shift"]) ** (data["Maturity"] - 1)) - 1)
 
@@ -48,11 +60,13 @@ import databricks.koalas as ks
 
 # Read in the data from csv
 data = ks.read_csv(rate_path)
-data = ks.read_csv(rate_path_group)
 
 # Shift the spot rate down along the maturity axis and calculate forward rate
-data["Spot_Rate_Shift"] = data.groupby("Currency")["Spot_Rate"].shift(1, fill_value=0)
+if select_scenario == "forward_rate_group":
+    data["Spot_Rate_Shift"] = data.groupby("Currency")["Spot_Rate"].shift(1, fill_value=0)
+else:
+    data["Spot_Rate_Shift"] = data["Spot_Rate"].shift(1, fill_value=0)
 data["Forward_Rate"] = (((1 + data["Spot_Rate"]) ** data["Maturity"]) /
                        ((1 + data["Spot_Rate_Shift"]) ** (data["Maturity"] - 1)) - 1)
 
-print("DONE")
+print("You have reached the end, well done!")
